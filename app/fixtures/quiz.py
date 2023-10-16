5e31d6a568c05ec51a0871658d20e999a9fba2fa
Jhoon Saravia <jhoonsar@gmail.com>
2023-10-16 23:08:26 +0200

    remove QUIZZES

--- app/fixtures/quiz.py
+++ app/fixtures/quiz.py
@@ -48,66 +48,66 @@
 #     }
 # ]
 
-QUIZZES = [
-    {
-        "title": "programming quiz",
-        "questions": [
-            {
-                "id": 1,
-                "question_text": "What data structure does a binary search tree degenerate to if it isn't balanced properly?",
-                "choices": [
-                    {"id": "a", "choice_text": "Queue"},
-                    {"id": "b", "choice_text": "Set"},
-                    {"id": "c", "choice_text": "Dictionary"},
-                    {"id": "d", "choice_text": "Linked List"},
-                ],
-                "correct": "d",
-            },
-            {
-                "id": 2,
-                "question_text": "What are the four pillars of Object-oriented programming?",
-                "choices": [
-                    {"id": "a", "choice_text": "Abstraction, Encapsulation, Inheritance, and Polymorphism"},
-                    {"id": "b", "choice_text": "Objects, Methods, Properties and Fields"},
-                    {"id": "c", "choice_text": "Immutability, Purity, Transparency and State"},
-                    {"id": "d", "choice_text": "Declaration, Modeling, Evaluation, and Implementation"},
-                ],
-                "correct": "a",
-            },
-            {
-                "id": 3,
-                "question_text": "What color would rgb(255,255,255) result in?",
-                "choices": [
-                    {"id": "a", "choice_text": "Red"},
-                    {"id": "b", "choice_text": "Blue"},
-                    {"id": "c", "choice_text": "White"},
-                    {"id": "d", "choice_text": "Black"},
-                ],
-                "correct": "c",
-            },
-            {
-                "id": 4,
-                "question_text": "How many bytes make up a Gigabyte?",
-                "choices": [
-                    {"id": "a", "choice_text": "1 quintillion bytes"},
-                    {"id": "b", "choice_text": "1 trillion bytes"},
-                    {"id": "c", "choice_text": "1 billion bytes"},
-                    {"id": "d", "choice_text": "1 hundred bytes"},
-                ],
-                "correct": "c",
-            },
+# QUIZZES = [
+#     {
+#         "title": "programming quiz",
+#         "questions": [
+#             {
+#                 "id": 1,
+#                 "question_text": "What data structure does a binary search tree degenerate to if it isn't balanced properly?",
+#                 "choices": [
+#                     {"id": "a", "choice_text": "Queue"},
+#                     {"id": "b", "choice_text": "Set"},
+#                     {"id": "c", "choice_text": "Dictionary"},
+#                     {"id": "d", "choice_text": "Linked List"},
+#                 ],
+#                 "correct": "d",
+#             },
+#             {
+#                 "id": 2,
+#                 "question_text": "What are the four pillars of Object-oriented programming?",
+#                 "choices": [
+#                     {"id": "a", "choice_text": "Abstraction, Encapsulation, Inheritance, and Polymorphism"},
+#                     {"id": "b", "choice_text": "Objects, Methods, Properties and Fields"},
+#                     {"id": "c", "choice_text": "Immutability, Purity, Transparency and State"},
+#                     {"id": "d", "choice_text": "Declaration, Modeling, Evaluation, and Implementation"},
+#                 ],
+#                 "correct": "a",
+#             },
+#             {
+#                 "id": 3,
+#                 "question_text": "What color would rgb(255,255,255) result in?",
+#                 "choices": [
+#                     {"id": "a", "choice_text": "Red"},
+#                     {"id": "b", "choice_text": "Blue"},
+#                     {"id": "c", "choice_text": "White"},
+#                     {"id": "d", "choice_text": "Black"},
+#                 ],
+#                 "correct": "c",
+#             },
+#             {
+#                 "id": 4,
+#                 "question_text": "How many bytes make up a Gigabyte?",
+#                 "choices": [
+#                     {"id": "a", "choice_text": "1 quintillion bytes"},
+#                     {"id": "b", "choice_text": "1 trillion bytes"},
+#                     {"id": "c", "choice_text": "1 billion bytes"},
+#                     {"id": "d", "choice_text": "1 hundred bytes"},
+#                 ],
+#                 "correct": "c",
+#             },
 
-            {
-                "id": 5,
-                "question_text": "What does GIF stand for?",
-                "choices": [
-                    {"id": "a", "choice_text": "Graphical International Format"},
-                    {"id": "b", "choice_text": "Graphics Interchange Format"},
-                    {"id": "c", "choice_text": "General Interchange Format"},
-                    {"id": "c", "choice_text": "Graphical Interconnected Functions"},
-                ],
-                "correct": "b",
-            },
-        ],
-    }
-]
+#             {
+#                 "id": 5,
+#                 "question_text": "What does GIF stand for?",
+#                 "choices": [
+#                     {"id": "a", "choice_text": "Graphical International Format"},
+#                     {"id": "b", "choice_text": "Graphics Interchange Format"},
+#                     {"id": "c", "choice_text": "General Interchange Format"},
+#                     {"id": "c", "choice_text": "Graphical Interconnected Functions"},
+#                 ],
+#                 "correct": "b",
+#             },
+#         ],
+#     }
+# ]
